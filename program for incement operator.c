627d183069872b6aeace1d2901a9adaa4eec6a24
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-18 18:05:28 +0530

    Add files via upload

---
+++ program for incement operator.c
@@ -0,0 +1,14 @@
+// title of program
+// program for increment 
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int i=10,j;
+// input statement
+j=i+++i;
+// output statement
+printf("j=%d\n",j);
+}
