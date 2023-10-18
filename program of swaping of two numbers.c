69a7fa85b8c589d18489887c074d8845d668e041
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-18 17:38:55 +0530

    Add files via upload

---
+++ program of swaping of two numbers.c
@@ -0,0 +1,16 @@
+// title of program
+// swaping of two numbers
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int a;
+//output statement
+printf("Enter the number");
+// input statement
+scanf("%d",&a);
+//output statement
+printf("%d\n",3-a);
+}
