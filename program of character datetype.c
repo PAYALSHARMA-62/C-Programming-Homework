00c2b1cfd8eea297c660166758ff3e7b4e636866
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-18 18:27:20 +0530

    Add files via upload

---
+++ program of character datetype.c
@@ -0,0 +1,23 @@
+// title of program
+// program for character 
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int n;
+// output statement
+printf("enter the number");
+// input statement
+scanf("%d",&n);
+// char declaration
+char c;
+// output statement
+printf("enter the char");
+// input statement
+scanf("%c%c",&c,&c);
+// output statement
+printf("the no is=%d\n",n);
+printf("the char is=%c\n",c);
+}
