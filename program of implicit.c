7b4763f3c23a22c5a1388718392ccaebcb27525a
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-19 17:47:37 +0530

    Add files via upload

---
+++ program of implicit.c
@@ -0,0 +1,14 @@
+// title of program
+// Program for implicit
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int a=7,b=2;
+float c; 
+c=a/b;
+// output statement
+printf("Ans=%.1f\n",c);
+}
