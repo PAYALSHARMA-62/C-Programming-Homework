ef4ecf43d7dd9507e861578debff3cdafe2cb740
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-19 18:54:08 +0530

    Add files via upload

---
+++ program to find divisibility of number by3.c
@@ -0,0 +1,20 @@
+// title of program
+// program to find divisibility by3 usig if
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int a;
+//output statement
+printf("Enter the number");
+// input statement
+scanf("%d",&a);
+// condition for simple if
+if(a%3==0)
+{
+//output statement
+printf("high");
+}
+}
