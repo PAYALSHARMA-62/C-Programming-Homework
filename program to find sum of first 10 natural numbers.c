a839b86cf44f0fb5ea0939657f63e89ed5aa2f38
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-19 22:20:10 +0530

    Add files via upload

---
+++ program to find sum of first 10 natural numbers.c
@@ -0,0 +1,19 @@
+// title of program
+// Program to find sum of first 10 natural numbers using for loop
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int i,sum;
+// condition of for loop
+for(i=1;i<=10;i++)
+{
+// input statement for cube
+sum=sum+i;
+// output statement
+printf("sum=sum+i\n",sum);
+printf("%d\n",i);
+}
+}
