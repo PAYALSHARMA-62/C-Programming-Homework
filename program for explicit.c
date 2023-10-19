85997f9f41ca1289ba3574ad10de3bfd2cb669ea
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-19 17:51:41 +0530

    Add files via upload

---
+++ program for explicit.c
@@ -0,0 +1,14 @@
+// title of program
+// Program for explicit
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int a=7,b=2;
+float c; 
+c=(float)a/(float)b;
+// output statement
+printf("Ans=%.1f\n",c);
+}
