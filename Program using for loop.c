90f00df2b983e36527364153ff4151f5178bd048
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-20 20:09:35 +0530

    Add files via upload

---
+++ Program using for loop.c
@@ -0,0 +1,17 @@
+// titleof program
+// program using for loop
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int i;
+// condition of for loop
+for(i=1;i<=10;i++)
+{
+// output statement
+printf("Akash");
+printf("%d\n",i);
+}
+}
