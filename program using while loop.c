8dc3a39c036fa10522167eea7b846373ab35d2fe
PAYALSHARMA-62 <146845595+PAYALSHARMA-62@users.noreply.github.com>
2023-10-20 20:14:36 +0530

    Add files via upload

---
+++ program using while loop.c
@@ -0,0 +1,18 @@
+// titleof program
+// program using while loop
+// header file
+#include<stdio.h>
+// function heading
+void main()
+{
+// variable declaration
+int i=0;
+// condition of while loop
+while(i<=10)
+{
+// output statement
+printf("Akash\n");
+printf("%d\n",i);
+i++;
+}
+}
